9c607df918cf240da502830ccd6ffc1962f11a05
federico-init <fefe.borrometi@gmail.com>
2023-11-14 11:34:04 +0100

    style: scss added + style reset + base style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import "./app.scss";
+
 function App() {
-  return <div className="Hello World"></div>;
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio 1</section>
+      <section>Portfolio 2</section>
+      <section>Portfolio 3</section>
+      <section>Contact</section>
+    </div>
+  );
 }
 
 export default App;
