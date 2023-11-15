016913d6392a4f6cc6b004e30eb4c5ddfba45aad
federico-init <fefe.borrometi@gmail.com>
2023-11-15 02:05:58 +0100

    feat: animations w/ framer motion + sidebar added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,20 @@
+import { motion } from "framer-motion";
 import "./navbar.scss";
+import Sidebar from "../sidebar/Sidebar";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       {/* SIDEBAR */}
+      <Sidebar />
       <div className="wrapper">
-        <span>Federico Dev</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Federico Dev
+        </motion.span>
         <div className="social">
           <a href="github.com/federico-init">
             <img src="/github.png" alt="github" />
